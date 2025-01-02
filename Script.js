a7308c0e62146f27a2402e4a3b783f424aa12a90
Hesprs <Hesprs@outlook.com>
2025-01-02 11:52:32 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,15 +4,15 @@ function resize() {
     var rectangle = document.getElementById("rectangle");
     var upper_cover = document.getElementById("upper_cover");
     if (window.innerWidth >= 767) {
-        main.style.margin = "20px 20px 20px " + (sidenav.offsetWidth + 40) + "px";
+        main.style.margin = "15px 15px 15px " + (sidenav.offsetWidth + 30) + "px";
         rectangle.style.width = (main.offsetWidth - 3) + "px";
-        rectangle.style.height = "calc(100% - 43px)";
-        upper_cover.style.height = 19.5 + "px";
+        rectangle.style.height = "calc(100% - 33px)";
+        upper_cover.style.height = 15 + "px";
     } else {
-        main.style.margin = (sidenav.offsetHeight + 40) + "px 20px 20px 20px";
-        rectangle.style.height = "calc(100% - 63px - " + sidenav.offsetHeight + "px)";
-        rectangle.style.width = "calc(100% - 43px)";
-        upper_cover.style.height = (sidenav.offsetHeight + 39.5) + "px";
+        main.style.margin = (sidenav.offsetHeight + 30) + "px 15px 15px 15px";
+        rectangle.style.height = "calc(100% - 53px - " + sidenav.offsetHeight + "px)";
+        rectangle.style.width = "calc(100% - 33px)";
+        upper_cover.style.height = (sidenav.offsetHeight + 40) + "px";
     }
 }
 
