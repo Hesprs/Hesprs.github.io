1dd7c321ec5731b27d0ab74aafefba626e87e430
Hesprs <Hesprs@outlook.com>
2025-01-03 21:15:35 +0800

    Add files via upload

--- Script.js
+++ Script.js
@@ -3,18 +3,18 @@ function resize() {
     var sidenav = document.getElementById("sidenav");
     var rectangle = document.getElementById("rectangle");
     var upper_cover = document.getElementById("upper_cover");
-    if (window.innerWidth >= 767) {
+    if (window.innerWidth > 767) {
         main.style.margin = "15px 15px 15px " + (sidenav.offsetWidth + 30) + "px";
-        rectangle.style.width = (main.offsetWidth - 3) + "px";
-        rectangle.style.height = "calc(100% - 33px)";
+        rectangle.style.width = main.offsetWidth + "px";
+        rectangle.style.height = "calc(100% - 30px)";
         upper_cover.style.height = 15 + "px";
     } else {
         main.style.margin = (sidenav.offsetHeight + 30) + "px 15px 15px 15px";
-        rectangle.style.height = "calc(100% - 48px - " + sidenav.offsetHeight + "px)";
-        rectangle.style.width = "calc(100% - 33px)";
+        rectangle.style.height = "calc(100% - 45px - " + sidenav.offsetHeight + "px)";
+        rectangle.style.width = "calc(100% - 30px)";
         upper_cover.style.height = (sidenav.offsetHeight + 30) + "px";
     }
 }
-
+window.addEventListener('DOMContentLoaded', resize);
 window.addEventListener('load', resize);
 window.addEventListener('resize', resize);
