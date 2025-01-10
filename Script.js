7154ddd797da5cf3fcabf13606a045e018f3485a
Hesprs <Hesprs@outlook.com>
2025-01-10 17:23:26 +0800

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,26 +1,3 @@
-function resize() {
-    const main = document.getElementById("main");
-    const sidenav = document.getElementById("sidenav");
-    const rectangle = document.getElementById("rectangle");
-    const upper_cover = document.getElementById("upper_cover");
-    const downer_cover = document.getElementById("downer_cover");
-    if (window.innerWidth > 767) {
-        main.style.margin = "15px 15px 15px " + (sidenav.offsetWidth + 30) + "px";
-        rectangle.style.width = main.offsetWidth + "px";
-        rectangle.style.height = "calc(100% - 30px)";
-        upper_cover.style.width = (main.offsetWidth - 20) + "px";
-        downer_cover.style.width = (main.offsetWidth - 20) + "px";
-        upper_cover.style.height = "15px";
-    } else {
-        main.style.margin = (sidenav.offsetHeight + 30) + "px 15px 15px 15px";
-        rectangle.style.height = "calc(100% - 45px - " + sidenav.offsetHeight + "px)";
-        rectangle.style.width = "calc(100% - 30px)";
-        upper_cover.style.width = (main.offsetWidth - 20) + "px";
-        downer_cover.style.width = (main.offsetWidth - 20) + "px";
-        upper_cover.style.height = (sidenav.offsetHeight + 30) + "px";
-    }
-}
-
 function toggle() {;
     if (this.checked) {
       document.body.classList.add('dark');
@@ -39,7 +16,4 @@ function default_LightDark() {
     dn.addEventListener('change', toggle);
 }
 
-window.addEventListener('DOMContentLoaded', resize);
 window.addEventListener('DOMContentLoaded', default_LightDark);
-window.addEventListener('load', resize);
-window.addEventListener('resize', resize);
